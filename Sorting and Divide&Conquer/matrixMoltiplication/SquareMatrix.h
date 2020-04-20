e93a5455401e38043f8b8520dc7223e1a8698fec
barabba1 <g.barabba@yahoo.com>
2020-04-20 23:40:34 +0100

    Simple matrix test class

--- Sorting and Divide&Conquer/matrixMoltiplication/SquareMatrix.h
+++ Sorting and Divide&Conquer/matrixMoltiplication/SquareMatrix.h
@@ -3,34 +3,36 @@
 
 #include <iostream>
 
-// assign to SquareMatrix only ints and SquareMatrix themselves for recursion!
+// assign to SquareMatrix only ints and SquareMatrices for recursion!
 // do not use it with any other type
 template<class T> class SquareMatrix {
-//    template<class T> friend SquareMatrix<T> operator+(const SquareMatrix, const SquareMatrix&);
-//    template<class U> friend SquareMatrix<U> operator-(const SquareMatrix, const SquareMatrix);
-//    template<class U> friend SquareMatrix<U> operator*(const SquareMatrix&, const SquareMatrix&);
 public:
     typedef T value_type;
 
-    SquareMatrix(){}//: a(0), b(0), c(0), d(0) {}
+    SquareMatrix(){}
+
+// useless constructor
 //    SquareMatrix(const T* input_array) {
 //        a = *input_array;
 //        b = *input_array[1];
 //        c = *input_array[2];
 //        d = *input_array[3];
 //    }
+    
     SquareMatrix(const T& first, const T& second, const T& third, const T& fourth) {
         a = first;
         b = second;
         c = third;
         d = fourth;
     }
+    
     SquareMatrix<T>(const SquareMatrix<T>& rhs) { // copy constructor
         a = rhs.getA();
         b = rhs.getB();
         c = rhs.getC();
         d = rhs.getD();
     }
+    
     SquareMatrix& operator=(const SquareMatrix rhs) {  // assignment operator
         if (&rhs != this) {
             a = rhs.getA();
@@ -41,8 +43,10 @@ public:
 
         return *this;
     }
+    
     ~SquareMatrix() {}  // destructor
-
+    
+    // getters and setters
     T getA() const {return a;}
     T getB() const {return b;}
     T getC() const {return c;}
@@ -59,9 +63,10 @@ private:
     T a, b, c, d;
 };
 
+// arithmetic operators implemented in SquareMatrix.cpp
 template<class T> SquareMatrix<T> operator+(const SquareMatrix<T>, const SquareMatrix<T>&);
 template<class T> SquareMatrix<T> operator-(const SquareMatrix<T>, const SquareMatrix<T>&);
-template<class T> SquareMatrix<T> operator*(const SquareMatrix<T>&, const SquareMatrix<T>&);
+template<class T> SquareMatrix<T> operator*(const SquareMatrix<T>&, const SquareMatrix<T>&);    // Strassen algorithm
 
 #include "SquareMatrix.cpp"
 #endif // SQUAREMATRIX_H_INCLUDED
