2ba311c391b0cf33330dd0785053e99623bd558f
barabba1 <g.barabba@yahoo.com>
2020-04-21 00:47:14 +0100

    selects n-th statistic without randomisation

--- Sorting and Divide&Conquer/dselect.cpp
+++ Sorting and Divide&Conquer/dselect.cpp
@@ -26,7 +26,6 @@ void choosePivot(vector<int>::iterator& beg, vector<int>::iterator& end,
 
     int random_number = rand() % (range - 1);
     pivot_position = beg + random_number;
-//    cout << "random pivot position = " << random_number << " and " << *pivot_position << endl;
 }
 
 
@@ -57,58 +56,13 @@ void quickSort(vector<int>::iterator& beg, vector<int>::iterator& end)
     choosePivot(beg, end, pivot_position);
 
     partitionAroundPivot(beg, end, pivot_position);
-//    cout << "comparisons: " << count_comparisons << endl;
 
     vector<int>::iterator after_pivot;
     pivot_position == end - 1 ? after_pivot = pivot_position : after_pivot = pivot_position + 1;
-//    if (size > 1) {
-//        for (vector<int>::iterator jter = beg; jter != after_pivot; ++jter)
-//            cout << "first half: " << *jter << endl;
-//        for (vector<int>::iterator jter = after_pivot; jter != end; ++jter)
-//            cout << "second half: " << *jter << endl;
-//    }
 
     quickSort(beg, after_pivot);
     quickSort(after_pivot, end);
 }
-
-
-//int rSelect(vector<int>::iterator& beg, vector<int>::iterator& end, const int target_position)
-//{
-//    // base case
-//    if (end - beg <= 1) return *beg;
-//
-//    // choose pivot randomly
-//    vector<int>::iterator pivot_position;
-//    choosePivot(beg, end, pivot_position);  // returns the pivot_position at random
-//
-//    vector<int>::iterator iterator_at_target_pos = beg + target_position;
-//
-//    // partition data array around pivot
-//    partitionAroundPivot(beg, end, pivot_position);
-//
-//    int target_minus_pivot = 0;
-//    for (vector<int>::iterator iter = beg; iter != pivot_position; iter++)
-//        --target_minus_pivot;
-//    if (-target_minus_pivot < target_position)  // this is: if (pivot - beg < target_position) ..
-//        target_minus_pivot += target_position - 1;
-//    else
-//        target_minus_pivot = 0;
-////    cout << "target_minus_pivot = " << target_minus_pivot << endl;
-////
-////    for (vector<int>::iterator jter = beg; jter != after_pivot; ++jter)
-////        cout << "first half: " << *jter << endl;
-////    for (vector<int>::iterator jter = after_pivot; jter != end; ++jter)
-////        cout << "second half: " << *jter << endl;
-//
-//    vector<int>::iterator after_pivot = pivot_position + 1;
-//
-//    if (*pivot_position == *iterator_at_target_pos) return *pivot_position;
-//    else if (pivot_position > iterator_at_target_pos) return rSelect(beg, after_pivot, target_position);
-//    else return rSelect(after_pivot, end, target_minus_pivot);
-//
-//    //return 0;           // it never gets here
-//}
 
 
 int dSelect(vector<int>::iterator& beg, vector<int>::iterator& end, const int target_position)
@@ -139,18 +93,6 @@ int dSelect(vector<int>::iterator& beg, vector<int>::iterator& end, const int ta
             medians.push_back(*(iter + (end - iter) / 2));
     }
 
-//    cout << "medians: " << endl;
-//    for (size_t i = 0; i != medians.size(); ++i) {
-//        cout << medians[i] << " ";
-//    }
-//    cout << endl;
-
-//    cout << "input data: " << endl;
-//    for (vector<int>::iterator iter = beg; iter != end; ++iter) {
-//        cout << *iter << " ";
-//    }
-//    cout << endl;
-
     int median_pivot_position;
     if (medians.size() % 2 == 0)
         median_pivot_position = medians.size() / 2 - 1;
@@ -172,25 +114,10 @@ int dSelect(vector<int>::iterator& beg, vector<int>::iterator& end, const int ta
         median_of_medians = dSelect(medians_beg, medians_end, median_pivot_position);
     }
 
-//    cout << "medians: " << endl;
-//    for (size_t i = 0; i != medians.size(); ++i) {
-//        cout << medians[i] << " ";
-//    }
-//    cout << " pivot= " << *(medians.begin() + median_pivot_position) <<
-//            " pivot position= " << median_pivot_position << endl;
-//    cout << "median of medians: " << median_of_medians << endl;
-
     vector<int>::iterator iterator_at_median_pivot_pos = medians.begin() + median_pivot_position;
     vector<int>::iterator pivot_position = find(beg, end, *iterator_at_median_pivot_pos);
-    // partition data array around pivot
-//    cout << "iterator at pivot in medians: " << *iterator_at_median_pivot_pos
-//         << " in input array: " << *pivot_position << endl;
 
     partitionAroundPivot(beg, end, pivot_position);
-//    cout << "input after partition around pivot: " << endl;
-//    for (vector<int>::iterator iter = beg; iter != end; ++iter) {
-//        cout << *iter << " ";
-//    } cout << endl;
 
     int target_minus_pivot = 0;
     for (vector<int>::iterator iter = beg; *iter != *pivot_position; iter++)
@@ -199,12 +126,6 @@ int dSelect(vector<int>::iterator& beg, vector<int>::iterator& end, const int ta
         target_minus_pivot += target_position - 1;
     else
         target_minus_pivot = 0;
-//////    cout << "target_minus_pivot = " << target_minus_pivot << endl;
-//////
-//////    for (vector<int>::iterator jter = beg; jter != after_pivot; ++jter)
-//////        cout << "first half: " << *jter << endl;
-//////    for (vector<int>::iterator jter = after_pivot; jter != end; ++jter)
-//////        cout << "second half: " << *jter << endl;
 
     vector<int>::iterator after_pivot = pivot_position + 1;
     vector<int>::iterator iterator_at_target_pos = beg + target_position;
@@ -213,8 +134,8 @@ int dSelect(vector<int>::iterator& beg, vector<int>::iterator& end, const int ta
     else return dSelect(after_pivot, end, target_minus_pivot);
 }
 
-
-int main(int argc, char** argv)
+// main function extracts the median, but could be any other position in the array
+int main(int argc, char** argv)  // accepts input text file name as argument, without the '.txt' suffix
 {
     string tail(".txt");
     string filename(argv[1] + tail);
@@ -227,7 +148,6 @@ int main(int argc, char** argv)
     if (input) {
         while ( getline(input, string_element) ) {
             int element = stoi(string_element);
-//            cout << element << endl;
             data_array.push_back(element);
         }
     }
@@ -244,14 +164,7 @@ int main(int argc, char** argv)
 
     int median = dSelect(beg, end, median_pos);
 
-//    for(vector<int>::const_iterator iter = data_array.begin(); iter != data_array.end(); ++iter) {
-//        cout << *iter << " ";
-//    }
-//    cout << endl;
-
-    //input.open(filename, fstream::out | fstream::trunc);
     cout << "Median of given array of integers is: " << median << " at position " << median_pos << endl;
-    //input.close();
 
     return 0;
 }
